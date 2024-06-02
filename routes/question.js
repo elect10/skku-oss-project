425084953ab703656544c9d2d7615549e8e899bf
yaongmeow <minnie_00@naver.com>
2024-06-02 12:48:37 +0900

    feat: question router

--- routes/question.js
+++ routes/question.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import dataBase from '../app.js';
 
 export const router = express.Router();
 
@@ -7,14 +8,34 @@ router.post('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    res.send('question/:id');
+    dataBase.query(`SELECT * FROM questions WHERE id = ${req.params.id}`, (err, result) => {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 router.delete('/:id', (req, res) => {
+    dataBase.query(`DELETE FROM questions WHERE id = ${req.params.id}`, (err, result) => {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.send(result);
+        }
+    });
     res.send('/:id');
 });
 
 router.put('/:id', (req, res) => {
+    dataBase.query(`UPDATE questions SET title = '${req.body.title}', content = '${req.body.content}' WHERE id = ${req.params.id}`, (err, result) => {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.send(result);
+        }
+    });
     res.send('/:id');
 });
 
