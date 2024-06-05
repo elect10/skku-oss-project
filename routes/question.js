85b1db2d6236e46ec850ca445d1e64dde35427f6
yaongmeow <minnie_00@naver.com>
2024-06-05 21:04:25 +0900

    feat: complete question api

--- routes/question.js
+++ routes/question.js
@@ -1,24 +1,53 @@
 import express from 'express';
 import dataBase from '../app.js';
+import { v4 as uuidv4 } from "uuid";
 
 export const router = express.Router();
 
 router.post('/', (req, res) => {
-    res.send('question');
+    const { title, content } = req.body;
+    dataBase.query(`INSERT INTO questions (id, writer, title, content) `
+        + `VALUES ('${uuidv4()}','${req.session.user.id}', '${title}', '${content}')`, (err, result) => {
+            if (err) {
+                res.sendStatus(500);
+            }
+            else {
+                res.redirect('/');
+            }
+        });
 });
 
 router.get('/:id', (req, res) => {
-    dataBase.query(`SELECT * FROM questions WHERE id = ${req.params.id}`, (err, result) => {
-        if (err) {
-            res.sendStatus(500);
-        } else {
-            res.send(result);
+
+    dataBase.query('SELECT questions.id, questions.title, questions.content FROM questions ' +
+        `WHERE questions.id = '${req.params.id}'`, (err, resultQ) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+            }
+            else {
+                resultQ = resultQ[0];
+                console.log("resQ: " + JSON.stringify(resultQ));
+                dataBase.query('SELECT answers.content FROM questions ' +
+                    'JOIN answers ON questions.id = answers.question_id ' +
+                    `WHERE questions.id = '${req.params.id}'`, (err, resultA) => {
+                        console.log(resultA);
+                        if (err) {
+                            console.log(err);
+                            res.sendStatus(500);
+                        } else {
+                            console.log(resultA);
+                            res.render('question', { question: resultQ, answers: resultA });
+                        }
+                    }
+                );
+            }
         }
-    });
+    );
 });
 
 router.delete('/:id', (req, res) => {
-    dataBase.query(`DELETE FROM questions WHERE id = ${req.params.id}`, (err, result) => {
+    dataBase.query(`DELETE FROM questions WHERE id = '${req.params.id}'`, (err, result) => {
         if (err) {
             res.sendStatus(500);
         } else {
@@ -38,6 +67,21 @@ router.put('/:id', (req, res) => {
     });
     res.send('/:id');
 });
+
+router.post('/:id/answer', (req, res) => {
+    const questionId = req.params.id;
+    const { content } = req.body;
+
+    dataBase.query(`INSERT INTO answers (id, writer, question_id, content) `
+        + `VALUES ('${uuidv4()}','${req.session.user.id}', '${questionId}', '${content}')`, (err, result) => {
+            if (err) {
+                res.sendStatus(500);
+            }
+            else {
+                res.redirect(`/question/${questionId}`);
+            }
+        });
+})
 
 
 
